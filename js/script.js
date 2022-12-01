5bd1836aa8399e0fc532f73225a22efc417ac9b8
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-01 16:03:10 +0100

    start

--- js/script.js
+++ js/script.js
@@ -1,25 +1,11 @@
 // Consegna:
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
-// ed emetto un messaggio in console con il numero della cella cliccata.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: Nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-
-
-// recupero il contenitore della griglia dal documento: 
-// const containerGrid = document.querySelector("div.grid");
-
-// creo il quadrato/casella
-// const gridSquare = document.createElement("div");
-
-// aggiungo la classe "square" che ho preparato in css per vederne il comportamento 
-// gridSquare.classList.add("square");
-
-// procedo ad "appenderlo" al parent 
-// containerGrid.append(gridSquare);
-
-// inserisco l'elemento square all'interno di un ciclo for per aggiungere il numero dei quadrati che mi serviranno (100)
 
 
 // richiamo il bottone dal documento allo script 
